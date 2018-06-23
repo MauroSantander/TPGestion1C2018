095d32be96f070f0ad77309133a508f9376afaf6
benjamenriquez <benjamenriquez@gmail.com>
2018-06-22 22:04:48 -0300

    a
    a

--- TP_GESTION.sql
+++ TP_GESTION.sql
@@ -1438,3 +1438,26 @@ BEGIN
 	GROUP BY ha.idHotel, ha.idHabitacion
 END
 GO
+
+CREATE PROCEDURE [PISOS_PICADOS].topClientesPorPuntos @anio INT, @trimestre INT
+AS
+BEGIN
+	SELECT TOP 5 fact.cliente, 
+	CAST((SUM(reng.total)/10 + 
+	SUM(CASE
+		WHEN DATEPART(QUARTER,fechaCheckIn) = DATEPART(QUARTER,fechaCheckOut)
+		THEN (DATEDIFF(DAY,es.fechaCheckIn,es.fechaCheckOut))*[PISOS_PICADOS].precioRegimen(re.codigoRegimen)/20
+		WHEN DATEPART(QUARTER,fechaCheckIn) < DATEPART(QUARTER,fechaCheckOut) and DATEPART(QUARTER,fechaCheckIn) < @trimestre
+		THEN DATEDIFF(DAY, DATEADD(qq, DATEDIFF(qq, 0, GETDATE()), 0), fechaCheckOut)
+		WHEN DATEPART(QUARTER,fechaCheckIn) < DATEPART(QUARTER,fechaCheckOut) and DATEPART(QUARTER,fechaCheckOut) > @trimestre
+		THEN DATEDIFF(DAY, fechaCheckIn, DATEADD (dd, -1, DATEADD(qq, DATEDIFF(qq, 0, GETDATE()) +1, 0)))
+		END))
+	as bigint) as puntos
+	FROM [PISOS_PICADOS].Factura as fact JOIN  [PISOS_PICADOS].RenglonFactura as reng on fact.numeroFactura = reng.numeroFactura,
+	[PISOS_PICADOS].Estadia as es, [PISOS_PICADOS].Reserva as re
+	WHERE re.codigoReserva = es.codigoReserva and re.idCliente = fact.cliente and
+	(DATEPART(YEAR,fact.fecha) = @anio and DATEPART(QUARTER,fact.fecha) = @trimestre)
+	GROUP BY fact.cliente
+	ORDER BY puntos DESC
+END
+GO
